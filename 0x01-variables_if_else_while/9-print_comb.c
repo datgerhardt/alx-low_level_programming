6aacc52545398d5dc5471066c8ee0f4caed4c1fe
Datsomor Gerhardt <datsomorg@gmail.com>
2022-07-04 22:42:37 -0700

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: printing number from 0 to 9 using putchar
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		if (num == '9')
+		{
+			break;
+		}
+		putchar(',');
+		putchar(' ');
+		num++;
+	}
+
+	putchar('\n');
+	return (0);
+}
