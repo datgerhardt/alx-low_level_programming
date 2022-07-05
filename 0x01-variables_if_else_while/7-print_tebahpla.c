487f359167962495792c12529cb2a8fcb05763dd
Datsomor Gerhardt <datsomorg@gmail.com>
2022-07-04 21:48:49 -0700

    smile mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: printing alphabet without e and q
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char i = 'z';
+
+	while (i >= 'a')
+	{
+		putchar(i);
+		i--;
+	}
+
+	putchar('\n');
+	return (0);
+}
